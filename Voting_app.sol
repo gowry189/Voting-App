12c653102b0e087b8ba7b347285c057c32017673
Gowry <gowrysugathan18@gmail.com>
2018-10-23 00:04:09 +0530

    Implemented Struct

---
+++ Voting_app.sol
@@ -0,0 +1,41 @@
+pragma solidity ^0.4.18;
+
+contract Voting{
+    //in the contract voting define a datatype structure for storing informations of the voter
+    struct voter{
+        address voterAddress;
+        uint tokenBought;
+        bytes32[] tokensUsedPerCandidate;
+    }
+    //mapping the candidate with the votes he recieved
+    mapping(bytes32=>uint)public votesReceived;
+    mapping(address=>voter)public votersInfo;
+    bytes32[] public candidateList;
+    uint totalToken;
+    uint balanceToken;
+    uint tokenPrice;
+    
+    //constructor which takes in necessary values and initialize all the defined variables
+    function Voting(uint tokens, uint pricePerToken, bytes32[] candidateName){
+        totalToken = tokens;
+        balanceToken = tokens;
+        tokenPrice = pricePerToken;
+        candidateList = candidateName;
+    }
+    
+    /*msg.sender gives the money the sender is willing to give. That value divided by the 
+    price per token gives the number of tokens the person can buy*/
+    function buy() payable returns(uint){
+        uint tokensToBuy = msg.value/tokenPrice;
+        votersInfo[msg.sender].voterAddress = msg.sender;
+        votersInfo[msg.sender].tokenBought += tokensToBuy;
+        balanceToken -= tokensToBuy;
+        return tokensToBuy;
+    }
+    
+    function totalVotesFor(bytes32 candidate) view returns(uint){
+        return votesReceived[candidate];
+    }
+    
+    function 
+} 

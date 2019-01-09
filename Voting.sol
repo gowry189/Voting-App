57e3a0a57cd0e96628ca4ac7ddcacc6ff20ded15
Gowry Sugathan <gowrysugathan18@gmail.com>
2019-01-09 18:46:41 +0530

    Correct the files: deleted unnecessary files and corrected the $indentations

--- Voting.sol
+++ Voting.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.4.18;
 contract Voting {
 
     /* a mappig is done with candidate name stored in array with the vote count of each*/ 
+    
+    
     mapping(bytes32 => uint8) public votesReceived; 
     bytes32[] public candidateList;
     
     /* a constructor to initialize the candidate list with candidate name*/
+    
+    
     function Voting (bytes32 [] candidateNames) {
         candidateList = candidateNames;
     }
     
     /*function which return the total number of votes a candidate has obtained*/
+    
+    
     function totalVotesFor(bytes32 candidate) view public returns(uint8){
         require (validCandidate(candidate));
         return votesReceived[candidate] += 1;
@@ -19,12 +25,16 @@ contract Voting {
     
     
     /*function that incremnet the vote count*/
+    
+    
     function voteForCandidate(bytes32 candidate) public {
         require(validCandidate(candidate));
         votesReceived[candidate] += 1;
     }
     
     /*function which check whether a candadate is valid or not*/
+    
+    
     function validCandidate(bytes32 candidate) view public returns(bool) {
         for(uint i=0; i<candidateList.length; i++) {
             if(candidateList[i] == candidate) {
--- VotingCopy.sol
+++ VotingCopy.sol
@@ -2,12 +2,18 @@ pragma solidity ^0.4.18;
 
 contract Voting{
     //in the contract voting define a datatype structure for storing informations of the voter
+    
+    
     struct voter{
         address voterAddress;
         uint tokensBought;
         bytes32[] tokensUsedPerCandidate;
     }
+    
+    
     //mapping the candidate with the votes he recieved
+    
+    
     mapping(bytes32=>uint)public votesReceived;
     mapping(address=>voter)public votersInfo;
     bytes32[] public candidateList;
@@ -16,6 +22,8 @@ contract Voting{
     uint tokensPrice;
     
     //constructor which takes in necessary values and initialize all the defined variables
+    
+    
     function Voting(uint tokens, uint pricePerTokens, bytes32[] candidateName){
         totalTokens = tokens;
         balanceTokens = tokens;
@@ -25,6 +33,8 @@ contract Voting{
     
     /*msg.sender gives the money the sender is willing to give. That value divided by the 
     price per token gives the number of tokens the person can buy*/
+    
+    
     function buy() payable returns(uint){
         uint tokensToBuy = msg.value/tokensPrice;
         votersInfo[msg.sender].voterAddress = msg.sender;
